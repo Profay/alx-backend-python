093900d2fc861337940cfe26725815a14a4108b6
PROFAY <Oyewaletimmy01@gmail.com>
2023-01-09 05:59:03 +0000

    update

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module contains add function of variable annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ function add of variable annotation
+    """
+    return a + b
